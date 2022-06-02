b5506785334b2c9b66ab6726526f797fdf9ff286
Anish <anishkakaiya@outlook.com>
2022-06-02 20:35:22 +0100

    pass test to add new note

--- notesView.test.js
+++ notesView.test.js
@@ -20,4 +20,20 @@
  
      expect(document.querySelectorAll('div.note').length).toEqual(2);
    });
+
+   it('adds a new note', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    const input = document.querySelector('#user-input')
+    input.value = 'test note'
+
+    const buttonEl = document.querySelector('#add-note-button')
+    buttonEl.click();
+
+    expect(document.querySelectorAll('div.note').length).toEqual(1);
+    expect(document.querySelectorAll('div.note')[0].innerText).toEqual('test note');
+  });
  });
